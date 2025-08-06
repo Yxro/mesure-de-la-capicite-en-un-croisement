1a07ff29e2a76eb970a41ae3b1745f396d1c8562
FIKRI Younes <86892662+Yxro@users.noreply.github.com>
2025-08-06 14:46:11 +0100

    mesure.ino
    ajout des commentaires

--- mesure.ino
+++ mesure.ino
@@ -1,27 +1,20 @@
-const int ROW_PIN = 2;  // D2 = ligne
-const int COL_PIN = 6;  // D6 = colonne
+const int ROW_PIN = 2;  // ligne selon le port digital de la carte arduino
+const int COL_PIN = 6;  // colonne selon le port digital de la carte arduino
 
 unsigned long measureCapacitance() {
-  // Décharge toute la capacité résiduelle
+  // Décharge toute la capacité 
   pinMode(COL_PIN, OUTPUT);
   digitalWrite(COL_PIN, LOW);
   delay(5);  // brève décharge
-
-  // Prépare la colonne en entrée (haute impédance)
   pinMode(COL_PIN, INPUT);
-
-  // Envoie un signal sur la ligne (charge la capacité)
   pinMode(ROW_PIN, OUTPUT);
   digitalWrite(ROW_PIN, HIGH);
-
-  // Mesure le temps que met COL_PIN à passer à HIGH
   unsigned long startTime = micros();
   while (digitalRead(COL_PIN) == LOW) {
-    if ((micros() - startTime) > 3000) break; // timeout pour éviter boucle infinie
+    if ((micros() - startTime) > 3000) break; 
   }
   unsigned long elapsed = micros() - startTime;
 
-  // Réinitialise ROW_PIN à LOW
   digitalWrite(ROW_PIN, LOW);
 
   return elapsed;
